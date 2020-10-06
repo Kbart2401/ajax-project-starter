4871b1a1331707c9971690d23f5461bdf8658c87
Tanner Schmutte <tannerschmutte@gmail.com>
2020-10-06 14:46:02 -0700

    phase 4

--- public/events.js
+++ public/events.js
@@ -2,27 +2,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     const handleClick = async () => {
-        // debugger;
-        document.querySelector('.loader').innerHTML = "Loading..."
-        const res = await fetch('http://localhost:3000/kitten/image')
-        const json = await res.json()
-        document.querySelector('.loader').innerHTML = ""
-        document.querySelector('.cat-pic').src = json.src
 
-        if (!res.ok) {
-            console.log('Something went wrong! Please try again!')
+        try {
+
+            document.querySelector('.loader').innerHTML = "Loading..."
+            const res = await fetch('http://localhost:3000/kitten/image')
+
+            if (!res.ok) throw res
+
+            const json = await res.json()
+            document.querySelector('.loader').innerHTML = ""
+            document.querySelector('.cat-pic').src = json.src
+
+
+        } catch (err) {
+            console.log('error', err)
             alert("Something went wrong! Please try again!");
         }
-        // } catch (err) {
-        //     console.log('error', err)
-        //     alert("Something went wrong! Please try again!");
-        // }
+
     }
-
-
+    
+    document
+    .getElementById('new-pic')
+    .addEventListener('click', handleClick)
+    
+    
+    let score = Number(document.querySelector('.score').innerHTML)
+    console.log(typeof(score))
 
     document
-        .getElementById('new-pic')
-        .addEventListener('click', handleClick)
+        .getElementById('upvote')
+        .addEventListener('click', () => {
+            document.querySelector('.score').innerHTML = ++score
+        })
+    document
+        .getElementById('downvote')
+        .addEventListener('click', () => {
+            document.querySelector('.score').innerHTML = --score
+        })
+    
 
 })
