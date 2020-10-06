5b7c77fa9a926745e679a57ad79edfcd902e8cb0
Kyle Barthelmes <kbart2401@gmail.com>
2020-10-06 16:56:40 -0400

    passing phase 3

--- public/events.js
+++ public/events.js
@@ -1,17 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-   
-    const handleClick = async () => {
-        const res = await fetch('/kitten/image')
-        const json = await res.json()
 
+
+    const handleClick = async () => {
+        // debugger;
+        document.querySelector('.loader').innerHTML = "Loading..."
+        const res = await fetch('http://localhost:3000/kitten/image')
+        const json = await res.json()
+        document.querySelector('.loader').innerHTML = ""
         document.querySelector('.cat-pic').src = json.src
 
-
+        if (!res.ok) {
+            console.log('Something went wrong! Please try again!')
+            alert("Something went wrong! Please try again!");
+        }
+        // } catch (err) {
+        //     console.log('error', err)
+        //     alert("Something went wrong! Please try again!");
+        // }
     }
-    
-    
-    
+
+
+
     document
         .getElementById('new-pic')
         .addEventListener('click', handleClick)
